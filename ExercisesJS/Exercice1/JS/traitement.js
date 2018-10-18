191b58711fb41282041d2936a85ad929857f16ef
drovi1533209 <drovi1533209@etu.cegepjonquiere.ca>
2018-10-18 11:02:13 -0400

    ExerciceJS1 Fin

--- ExercisesJS/Exercice1/JS/traitement.js
+++ ExercisesJS/Exercice1/JS/traitement.js
@@ -1,5 +1,10 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var PdLbs, PdKg;
+
+ 	PdLbs = parseFloat(document.getElementById("PdLbs").value);
+    PdKg = PdLbs/2.2;
+
+    console.log("Votre poids en Kilogramme est "+PdKg);
 
 }
